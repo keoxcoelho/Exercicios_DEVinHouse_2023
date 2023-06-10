bb34c7873e060b00c2fa77c30a122e951760b62d
KEO <keoxcoelho@gmail.com>
2023-06-10 12:53:49 -0300

    criacao do exercicio [M1S04] Ex 5 - Média Ponderada

---
+++ [M1S04] Ex 5 - Média Ponderada.js
@@ -0,0 +1,2 @@
+// Peça ao usuário que digite três notas e seus respectivos pesos. Em seguida, escreva um programa em JavaScript que calcule a média ponderada dessas notas. A média ponderada é calculada multiplicando cada nota pelo seu peso correspondente, somando todos os resultados e dividindo pelo somatório dos pesos. Exiba o resultado na tela. //
+
