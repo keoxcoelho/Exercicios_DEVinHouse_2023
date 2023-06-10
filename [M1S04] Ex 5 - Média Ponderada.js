839f4e1457da0d6238fbcda237d627a291df409b
KEO <keoxcoelho@gmail.com>
2023-06-10 13:12:45 -0300

    add exemplo de objeto

--- [M1S04] Ex 5 - Média Ponderada.js
+++ [M1S04] Ex 5 - Média Ponderada.js
@@ -16,3 +16,22 @@ let somaPesos = p1 + p2 + p3;
 let mediaPonderada = somaProdutos / somaPesos;
 
 alert("A média ponderada das notas é " + mediaPonderada);
+
+// exemplo que o Henrique Coelho fez com objeto:
+
+let prova1 = {
+    nota: parseInt(prompt("Digite a nota da prova 1:")),
+    peso: parseInt(prompt("Digite o peso da nota 1:"))
+} 
+
+let prova2 = {
+    nota: parseInt(prompt("Digite a nota da prova 2:")),
+    peso: parseInt(prompt("Digite o peso da nota 2:"))
+}
+
+let prova3 = {
+    nota: parseInt(prompt("Digite a nota da prova 3:")),
+    peso: parseInt(prompt("Digite o peso da nota 3:"))
+}
+
+let mediaPonderada = alert(((prova1.nota * prova1.peso) + (prova2.nota * prova2.peso) + (prova3.nota * prova3.peso)) / (prova1.peso + prova2.peso + prova3.peso)); // 
---
+++ [M1S04] Ex 6 - Concatenação de nomes.js
@@ -0,0 +1,8 @@
+// Faça um programa em JavaScript que solicite ao usuário que digite seu primeiro nome e seu sobrenome. Em seguida, eles devem utilizar operadores de string para concatenar os dois nomes e exibir o nome completo na tela.//
+
+let nome = prompt("Digite seu nome: ");
+let sobrenome = prompt("Digite seu sobrenome: ");
+
+let nomeCompleto = nome + " " + sobrenome;
+
+alert(nomeCompleto);
